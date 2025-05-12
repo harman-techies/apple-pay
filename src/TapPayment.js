4b31acbe33fcff9228071b0d6c168c90b8206973
harman-techies <harmanpreet.singh@iamtechie.com>
2025-05-12 10:05:50 +0530

    updated some code

--- src/TapPayment.js
+++ src/TapPayment.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function TapPayment() {
   useEffect(() => {
@@ -10,9 +11,9 @@ export default function TapPayment() {
       window.CardSDK;
 
     renderTapCard("card-sdk-id", {
-      publicKey: "pk_test_SJDg4fFZcYW9qQKaXL3l6MpT",
+      publicKey: "pk_test_C8fBZz15JEYR9peFrQitPbmK",
       merchant: {
-        id: "65435453",
+        id: 65435453,
       },
       transaction: {
         amount: 1,
@@ -36,16 +37,65 @@ export default function TapPayment() {
         edges: Edges.CURVED,
         direction: Direction.LTR,
       },
-      onReady: () => console.log("onReady"),
-      onFocus: () => console.log("onFocus"),
-      onBinIdentification: (data) => console.log("onBinIdentification", data),
-      onValidInput: (data) => console.log("onValidInputChange", data),
-      onInvalidInput: (data) => console.log("onInvalidInput", data),
-      onChangeSaveCardLater: (isSaveCardSelected) =>
-        console.log(isSaveCardSelected, " :onChangeSaveCardLater"), // isSaveCardSelected:boolean
-      onError: (data) => console.log("onError", data),
-      onSuccess: (data) => console.log("onSuccess", data),
+      onReady: () => {},
+      onFocus: () => {},
+      onBinIdentification: (data) => {},
+      onValidInput: (data) => {},
+      onInvalidInput: (data) => {},
+      onChangeSaveCardLater: (isSaveCardSelected) => {},
+      onError: (data) => {
+        console.log("onError", data);
+      },
+      onSuccess: (data) => {
+        console.log("onSuccess", data);
+        ChargePayment(data.id);
+      },
     });
+  };
+
+  const ChargePayment = (token) => {
+    const { Currencies } = window.CardSDK;
+    const data = {
+      amount: 1,
+      currency: Currencies.SAR,
+      customer_initiated: true,
+      threeDSecure: true,
+      save_card: false,
+      description: "Test Description",
+      metadata: { udf1: "Metadata 1" },
+      receipt: { email: false, sms: false },
+      reference: { transaction: "txn_01", order: "ord_01" },
+      customer: {
+        first_name: "test",
+        middle_name: "test",
+        last_name: "test",
+        email: "test@test.com",
+        phone: { country_code: 966, number: 987654321 },
+      },
+      merchant: { id: 65435453 },
+      source: { id: token },
+      post: {
+        url: "https://techies-infotech.netlify.app/post_url",
+      },
+      redirect: {
+        url: "https://techies-infotech.netlify.app/redirect_url",
+      },
+    };
+
+    const headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer sk_test_g9NP3YqdhHJRsZy2Au4letEk",
+    };
+
+    axios
+      .post("https://api.tap.company/v2/charges/", data, { headers })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error.response ? error.response.data : error.message);
+      });
   };
 
   return (
@@ -55,6 +105,8 @@ export default function TapPayment() {
         <button id="card-v2" onClick={() => window.CardSDK.tokenize()}>
           Pay Now
         </button>
+
+        <button onClick={() => ChargePayment()}>Create a Charge</button>
       </div>
     </div>
   );
